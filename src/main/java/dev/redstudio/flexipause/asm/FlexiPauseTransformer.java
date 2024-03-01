6d0f1b62c571a6c4ff369908853b38ee0b9a1a19
Desoroxxx <desoroxxx@gmail.com>
2024-03-01 09:58:46 +0100

    Fixed not being able to tweak other mods classes

--- src/main/java/dev/redstudio/flexipause/asm/FlexiPauseTransformer.java
+++ src/main/java/dev/redstudio/flexipause/asm/FlexiPauseTransformer.java
@@ -11,8 +11,7 @@ import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static dev.redstudio.flexipause.ProjectConstants.LOGGER;
 
@@ -28,7 +27,8 @@ import static dev.redstudio.flexipause.ProjectConstants.LOGGER;
  */
 public final class FlexiPauseTransformer implements IClassTransformer {
 
-    private static final String methodName = FMLLaunchHandler.isDeobfuscatedEnvironment() ? "doesGuiPauseGame" : "d";
+    private static final String MCP_OR_NOTCH_METHOD_NAME = FMLLaunchHandler.isDeobfuscatedEnvironment() ? "doesGuiPauseGame" : "d";
+    private static final String SEARGE_METHOD_NAME =  "func_73868_f";
 
     private static Map<String, Boolean> pauseOverrides;
 
@@ -41,8 +41,22 @@ public final class FlexiPauseTransformer implements IClassTransformer {
 
         new ClassReader(basicClass).accept(classNode, 0);
 
-        classNode.methods.removeIf(method -> method.name.equals(methodName) && method.desc.equals("()Z"));
-        classNode.methods.add(createMethod(pauseOverrides.get(transformedName)));
+        boolean matchedSeargeMethodName = false;
+
+        final Iterator<MethodNode> iterator = classNode.methods.iterator();
+        while (iterator.hasNext()) {
+            final MethodNode method = iterator.next();
+
+            if (!((method.name.equals(MCP_OR_NOTCH_METHOD_NAME) || method.name.equals(SEARGE_METHOD_NAME)) && method.desc.equals("()Z")))
+                continue;
+
+            if (method.name.equals(SEARGE_METHOD_NAME))
+                matchedSeargeMethodName = true;
+
+            iterator.remove();
+        }
+
+        classNode.methods.add(createMethod(matchedSeargeMethodName, pauseOverrides.get(transformedName)));
 
         final ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
 
@@ -54,10 +68,13 @@ public final class FlexiPauseTransformer implements IClassTransformer {
     /**
      * Creates a {@link GuiScreen#doesGuiPauseGame} with a hardcoded return value.
      *
+     * @param useSeargeMethodName Whether the name of the created {@link GuiScreen#doesGuiPauseGame} method should use {@link #SEARGE_METHOD_NAME} or {@link #MCP_OR_NOTCH_METHOD_NAME}
      * @param shouldPauseGame The hardcoded return value determining if the game should be paused
      * @return The newly created {@link GuiScreen#doesGuiPauseGame} method
      */
-    private MethodNode createMethod(final boolean shouldPauseGame) {
+    private MethodNode createMethod(final boolean useSeargeMethodName, final boolean shouldPauseGame) {
+        final String methodName = useSeargeMethodName ? SEARGE_METHOD_NAME : MCP_OR_NOTCH_METHOD_NAME;
+
         final MethodNode method = new MethodNode(Opcodes.ACC_PUBLIC, methodName, "()Z", null, null);
 
         method.instructions.add(new InsnNode(shouldPauseGame ? Opcodes.ICONST_1 : Opcodes.ICONST_0));
